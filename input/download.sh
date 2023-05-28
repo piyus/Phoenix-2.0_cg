91668409a44cf086c60536dd15d979b7b5d58c68
piyus kedia <piyuskedia@gmail.com>
2023-05-28 08:18:42 +0530

    added Defines.mk

--- input/download.sh
+++ input/download.sh
@@ -1,11 +1,29 @@
-wget http://csl.stanford.edu/~christos/data/histogram.tar.gz
-wget http://csl.stanford.edu/~christos/data/linear_regression.tar.gz
-wget http://csl.stanford.edu/~christos/data/word_count.tar.gz
+FILE1=`find ../../../ -iname large.bmp`
+FILE2=`find ../../../ -iname key_file_500MB.txt`
+FILE3=`find ../../../ -iname word_100MB.txt`
 
-tar -xvf histogram.tar.gz
-tar -xvf linear_regression.tar.gz
-tar -xvf word_count.tar.gz
+if [ -n "$FILE1" ]; then
+  cp $FILE1 .
+else
+  wget http://csl.stanford.edu/~christos/data/histogram.tar.gz
+  tar -xvf histogram.tar.gz
+  mv histogram_datafiles/large.bmp .
+fi
 
-mv histogram_datafiles/large.bmp .
-mv linear_regression_datafiles/key_file_500MB.txt .
-mv word_count_datafiles/word_100MB.txt .
+
+if [ -n "$FILE2" ]; then
+  cp $FILE2 .
+else
+  wget http://csl.stanford.edu/~christos/data/linear_regression.tar.gz
+  tar -xvf linear_regression.tar.gz
+  mv linear_regression_datafiles/key_file_500MB.txt .
+fi
+
+if [ -n "$FILE3" ]; then
+  cp $FILE3 .
+else
+  wget http://csl.stanford.edu/~christos/data/word_count.tar.gz
+  tar -xvf histogram.tar.gz
+  tar -xvf word_count.tar.gz
+  mv word_count_datafiles/word_100MB.txt .
+fi
